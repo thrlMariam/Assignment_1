48cc1235f61e8b8284db7daf6f74e2816e2a72e5
Rhegan Fortuin <kingrhegan1234@gmail.com>
2021-03-31 17:48:13 +0200

    Add getters and setters

--- src/main/java/za/ac/cput/assignment_1/Assignment_1.java
+++ src/main/java/za/ac/cput/assignment_1/Assignment_1.java
@@ -10,5 +10,35 @@ package za.ac.cput.assignment_1;
  * @author Rhegan
  */
 public class Assignment_1 {
+    public String id, name, accountType;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment_1{" + "id=" + id + ", name=" + name + ", accountType=" + accountType + '}';
+    }
     
 }
